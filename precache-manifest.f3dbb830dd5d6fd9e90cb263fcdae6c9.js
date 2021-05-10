97f9ba873b05b088e3b0dcb68ae21b1120ff85a0
OrdersFinance <ordersfinance@protonmail.com>
2021-05-10 12:51:24 +0200

    Updates

---
+++ precache-manifest.f3dbb830dd5d6fd9e90cb263fcdae6c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e68110da73a593c762e66fdce515a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d50ecdde9cd25052f2eb",
+    "url": "/static/css/main.0844ef33.chunk.css"
+  },
+  {
+    "revision": "5034eb7d74ee965563ce",
+    "url": "/static/js/2.360e5726.chunk.js"
+  },
+  {
+    "revision": "d50ecdde9cd25052f2eb",
+    "url": "/static/js/main.44bc5f7d.chunk.js"
+  },
+  {
+    "revision": "092de7e91814cc51d043",
+    "url": "/static/js/runtime~main.b57c6a77.js"
+  },
+  {
+    "revision": "1262baa005bb9726ddf5eff5377daf50",
+    "url": "/static/media/feature-tile-icon-04.1262baa0.svg"
+  },
+  {
+    "revision": "053e1597992dac8cdb3325326ebc2519",
+    "url": "/static/media/feature-tile-icon-05.053e1597.svg"
+  },
+  {
+    "revision": "909aee247a43cef604779ca2eae55f41",
+    "url": "/static/media/illustration-section-01.909aee24.svg"
+  },
+  {
+    "revision": "ce3e4b9a700c3f00b743c77d842eb17b",
+    "url": "/static/media/illustration-section-02.ce3e4b9a.svg"
+  },
+  {
+    "revision": "0dd8111837650272d69b4799275d9f31",
+    "url": "/static/media/logo.0dd81118.svg"
+  }
+]);
